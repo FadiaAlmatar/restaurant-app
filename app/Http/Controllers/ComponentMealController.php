b3286ffe461660ac1a78b2d54fef5beec3f4b77d
Your Name <you@example.com>
2021-06-02 00:39:43 +0300

    auth and model

---
+++ app/Http/Controllers/ComponentMealController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ComponentMeal;
+use Illuminate\Http\Request;
+
+class ComponentMealController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ComponentMeal  $componentMeal
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ComponentMeal $componentMeal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ComponentMeal  $componentMeal
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ComponentMeal $componentMeal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ComponentMeal  $componentMeal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ComponentMeal $componentMeal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ComponentMeal  $componentMeal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ComponentMeal $componentMeal)
+    {
+        //
+    }
+}
---
+++ database/factories/ComponentMealFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ComponentMeal;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ComponentMealFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ComponentMeal::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
